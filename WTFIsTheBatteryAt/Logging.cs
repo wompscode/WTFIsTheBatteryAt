661f14e19b40a9acbd9bd84286a6d76f58e2e0d4
womp <24914325+wompscode@users.noreply.github.com>
2025-01-14 21:37:43 +1100

    1.1.0: Form1.cs: Fix bug with spam of icon generation causing flickering in tray. *: Improve debug logging.

--- WTFIsTheBatteryAt/Logging.cs
+++ WTFIsTheBatteryAt/Logging.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 namespace WTFIsTheBatteryAt
 {
-    internal static class Logging
+    static class Logging
     {
-        public static void Log(string text = "This string will never ever be shown. In fact, it's entire existence is just a placeholder to know that the user hasn't entered anything in. I could really write whatever I wanted in here, couldn't I? Probably. Does it matter? Does anything matter? Oh god.")
+        public static void Log(string text = "This string will never ever be shown. In fact, it's entire existence is just a placeholder to know that the user hasn't entered anything in. I could really write whatever I wanted in here, couldn't I? Probably. Does it matter? Does anything matter? Oh god.", string prefix = "[debug]")
         {
             if (!Program.debug) return;
             if (text == null) return;
-
             if (text == "This string will never ever be shown. In fact, it's entire existence is just a placeholder to know that the user hasn't entered anything in. I could really write whatever I wanted in here, couldn't I? Probably. Does it matter? Does anything matter? Oh god.") return;
 
             DateTime current = DateTime.Now;
-            Console.WriteLine($"{current.ToShortDateString()} {current.ToLongTimeString()}: {text}");
+            Console.WriteLine($"{prefix} {current.ToShortDateString()} {current.ToLongTimeString()}: {text}");
         }
     }
 }
